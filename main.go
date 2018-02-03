02605e446a0e1b62ee5c75083270e938d6fafcb1
tamal <tamal@appscode.com>
2018-02-03 04:54:42 -0800

    added-all

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"path/filepath"
+
+	"github.com/appscode/go/log"
+	"github.com/tamalsaha/go-oneliners"
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+	masterURL := ""
+	kubeconfigPath := filepath.Join(homedir.HomeDir(), ".kube/config")
+
+	config, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfigPath)
+	if err != nil {
+		log.Fatalf("Could not get Kubernetes config: %s", err)
+	}
+
+	kc := kubernetes.NewForConfigOrDie(config)
+	oneliners.FILE(kc.CoreV1().Nodes())
+}
